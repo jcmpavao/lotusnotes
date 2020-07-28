5a62995ac0503c10ef8d1572db6ec30fde3464bb
João Carlos <jcmpavao@users.noreply.github.com>
2020-07-28 19:21:25 -0300

    Versão para commite

--- servico.py
+++ servico.py
@@ -253,5 +253,5 @@ def getLei(id):
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5002))
-	app.run(host='192.168.5.66', port=port)
+	port = int(os.environ.get("PORT", 5001))
+	app.run(host='0.0.0.0', port=port)
